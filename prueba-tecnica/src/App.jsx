5854259045ee381267b570eb156372a2ff66fec6
Diegozzzp <diegoadan.mejias@gmail.com>
2024-11-18 21:32:39 -0400

    Update App.jsx

--- prueba-tecnica/src/App.jsx
+++ prueba-tecnica/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBar";
 import Home from "./pages/Home"; // Asegúrate de tener estas rutas y componentes
 import Search from "./pages/Search";
 import Create from "./pages/CreateEdit";
